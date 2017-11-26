e7f4731d3d278ab32fe6e7ead0711fc7e44a4ada
iqno <hefelle@gmail.com>
2017-11-26 10:06:34 +0100

    es6

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 /* eslint-disable no-console */
-export default function log() {
+//log
+export default () => {
   if (console && console.log) {
     console.log.apply(console, [...arguments]);
   }
-}
+};
 
-export function info() {
+export const info = () => {
   if (console && console.info) {
     console.info.apply(console, [...arguments]);
   }
-}
+};
 
-export function warn() {
+export const warn = () => {
   if (console && console.warn) {
     console.warn.apply(console, [...arguments]);
   }
-}
+};
 
-export function error() {
+export const error = () => {
   if (console && console.error) {
     console.error.apply(console, [...arguments]);
   }
-}
+};
 
-export function dir() {
+export const dir = () => {
   if (console && console.dir) {
     console.dir.apply(console, [...arguments]);
   }
-}
+};
 /* eslint-enable no-console */
